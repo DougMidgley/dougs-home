7fc2845341988dd99f8bf4b1c709fe12c7343236
Doug Midgley <Doug Midgley>
2017-03-05 23:53:05 +0100

    mongoose

--- server.js
+++ server.js
@@ -69,6 +69,7 @@ app.post("/api/makeschema", function(req, res) {
     });
     // upserts
     var settings = mongoose.model('settings', settingsSchema);
+    console.log(settings);
     res.status(200).json(settings);
 
 });
