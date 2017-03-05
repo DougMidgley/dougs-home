848b8f5c31bded8eaae9db4a8da93702dbcb82ae
Doug Midgley <Doug Midgley>
2017-03-05 16:32:37 +0100

    correction

--- server.js
+++ server.js
@@ -53,25 +53,12 @@ function handleError(res, reason, message, code) {
   res.status(code || 500).json({"error": message});
 }
 
-/*  "/api/contacts"
- *    GET: finds all contacts
- *    POST: creates a new contact
- */
-
-app.get("/api/contacts", function(req, res) {
-});
-
-app.post("/api/contacts", function(req, res) {
-});
 
 /*  "/api/contacts/:id"
  *    GET: find contact by id
  *    PUT: update contact by id
  *    DELETE: deletes contact by id
  */
-
-app.get("/api/contacts/:id", function(req, res) {
-});
 
 app.put("/api/contacts/:id", function(req, res) {
 });
