0c1d3896360825ec0448d95074af8b1016b3fc02
Doug Midgley <douglasmidgley@gmail.com>
2017-07-02 21:39:39 +0200

    something

---
+++ routes/graph.js
@@ -0,0 +1,106 @@
+var mongoose = require('mongoose'),
+	router = require('express').Router(),
+	request = require('request'),
+    Model_Settings = require("../Models/Model_Settings.js"),
+    Model_Data = require("../Models/Model_Data.js");
+
+console.log('settings loaded');
+
+{
+  series: [
+    {
+      name: 'series-1',
+      data: [
+        {x: new Date(143134652600), y: 53},
+        {x: new Date(143234652600), y: 40},
+        {x: new Date(143340052600), y: 45},
+        {x: new Date(143366652600), y: 40},
+        {x: new Date(143410652600), y: 20},
+        {x: new Date(143508652600), y: 32},
+        {x: new Date(143569652600), y: 18},
+        {x: new Date(143579652600), y: 11}
+      ]
+    }
+  ]
+}
+
+router.get("/chartdata", function(req, res) {
+    var filtervalue = "";
+    if(req.query !== 'undefined' ){
+        //filtervalue = req.body; 
+        console.log(req.query);
+        filtervalue = req.query;
+    }
+
+    // retrieve the model 
+    //get req
+     var query = Model_Data.find(filtervalue, function(err, doc) {
+            if (err) throw err;
+            console.log('doc');
+            console.log(doc); 
+            //res.render('Raw', { title: 'Heres your JSON Response', message: doc});
+            var parseddata = parsemongodata(doc);
+            res.status(200).json(parseddata);
+        });
+
+});
+
+function parsemongodata(doc){
+    console.log(doc.length);
+    var data= {};
+    for (i = 0; i<doc.length;i++) {
+        var sensor = doc[i].sensorname;
+        if (data[sensor]){
+            data[sensor] = data[sensor] + doc[i].value;
+        } else {
+            data[sensor] = doc[i].value;
+        }
+    }
+    console.log(data);
+    return data;
+}
+
+router.get("/timeseries", function(req, res) {
+    var filtervalue = "";
+    if(req.query !== 'undefined' ){
+        //filtervalue = req.body; 
+        console.log(req.query);
+        filtervalue = req.query;
+    }
+
+    // retrieve the model 
+    //get req
+     var query = Model_Data.find(filtervalue, function(err, doc) {
+            if (err) throw err;
+            console.log('doc');
+            console.log(doc); 
+            //res.render('Raw', { title: 'Heres your JSON Response', message: doc});
+            var parseddata = parsefortimeseries(doc);
+            res.status(200).json(parseddata);
+        });
+
+});
+
+function parsefortimeseries(doc){
+    console.log(doc.length);
+    var data= {series: []};
+    var allsensors = doc.map(function(a) {return a.sensorname;});
+    var uniquesensors = allsensors.filter((x, i, a) => a.indexOf(x) == i);
+
+    var filteredObjects = doc.filter(function(d){
+    return d.sensorname == "temp";
+    })
+
+    for (i = 0; i<filteredObjects.length;i++) {
+        var sensor = filteredObjects[i].sensorname;
+        if (data[sensor]){
+            data[sensor] = data[sensor] + doc[i].value;
+        } else {
+            data[sensor] = doc[i].value;
+        }
+    }
+    console.log(data);
+    return data;
+}
+
+module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -2,5 +2,6 @@ var router = require('express').Router();
 
 router.use('/weather', require('./weather.js'));
 router.use('/api', require('./api.js'));
+router.use('/graph', require('./graph.js'));
 
 module.exports = router;
