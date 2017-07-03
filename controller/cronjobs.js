0df9ef9b6ae12c00fd71944045ed6de198ff3ab9
Doug Midgley <douglasmidgley@gmail.com>
2017-07-03 19:10:16 +0200

    something

--- controller/cronjobs.js
+++ controller/cronjobs.js
@@ -1,11 +1,12 @@
 var cron = require('node-cron');
+var weather = require('./controller/weather.js');
 
 
 module.exports = {
   runJobs: function() {
 	cron.schedule('* * * * *', function(){
 	  console.log('running a task every minute');
-	  console.log(getweather());
+	  console.log(weather.getweather());
 
 	});
 	
