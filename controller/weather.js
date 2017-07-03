e4cb5db83e6d3a40e601502f49bd1bad666a6b28
Doug Midgley <douglasmidgley@gmail.com>
2017-07-03 19:28:56 +0200

    something

--- controller/weather.js
+++ controller/weather.js
@@ -7,16 +7,16 @@ module.exports = {
   	var cityid = "2925533";
   	request
   	.get("http://api.openweathermap.org/data/2.5/forecast?id=" + cityid + "&APPID=" + apikey)
-	.on('response', function(data) {
+	.on('data', function(data) {
 		console.log('success', data);
 		    var currentweather = data.list[0];
-		    var response = {
+		    var SensorObject = {
 		    	sensorname: "Current Temperature", 
 		    	sensorunits: "Celcius", 
 		    	value: TempConvert.k2c(currentweather.main.temp), 
 		    	DateTime: currentweather.dt};
-		    console.log(response);
-			return response;
+		    console.log(SensorObject);
+			return SensorObject;
 	  })
 	.on('error', function(err) {
     console.log(err)
