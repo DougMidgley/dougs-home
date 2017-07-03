99d2a63f262e091e391a0d6dd7210b5d993f8754
Doug Midgley <douglasmidgley@gmail.com>
2017-07-03 19:23:22 +0200

    something

--- controller/weather.js
+++ controller/weather.js
@@ -1,29 +1,26 @@
 // contents of utils.js
 var TempConvert = require('temp-units-conv');
-var $ = require('jquery');
+var request = require('request');
 module.exports = {
   getweather: function() {
   	var apikey = process.env.openweatherkey;
   	var cityid = "2925533";
-  $.ajax({
-    url: "http://api.openweathermap.org/data/2.5/forecast?id=" + cityid + "&APPID=" + apikey,
-    method: "GET"}
-    )
-  .done(function(data) {
-    console.log('success', data);
-    var currentweather = data.list[0];
-    var response = {
-    	sensorname: "Current Temperature", 
-    	sensorunits: "Celcius", 
-    	value: TempConvert.k2c(currentweather.main.temp), 
-    	DateTime: currentweather.dt};
-    console.log(response);
-	return response;
-  
+  	request
+  	.get("http://api.openweathermap.org/data/2.5/forecast?id=" + cityid + "&APPID=" + apikey)
+	.on('response', function(data) {
+		console.log('success', data);
+		    var currentweather = data.list[0];
+		    var response = {
+		    	sensorname: "Current Temperature", 
+		    	sensorunits: "Celcius", 
+		    	value: TempConvert.k2c(currentweather.main.temp), 
+		    	DateTime: currentweather.dt};
+		    console.log(response);
+			return response;
+	  })
+	.on('error', function(err) {
+    console.log(err)
   })
-  .fail(function(xhr) {
-    console.log('error', xhr);
-  });
 	
   }
 }
