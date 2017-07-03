171449664d6fb3672414e67a7b851f0b27b99c48
Doug Midgley <douglasmidgley@gmail.com>
2017-07-03 21:03:44 +0200

    something

--- controller/weather.js
+++ controller/weather.js
@@ -6,36 +6,31 @@ module.exports = {
   getweather: function() {
   	var apikey = process.env.openweatherkey;
   	var cityid = "2925533";
-  	var url = "http://api.openweathermap.org/data/2.5/forecast?id=" + cityid + "&APPID=" + apikey;
-
-  	request.get(url, function(res){
-    res.setEncoding('utf8');
-    var JSONString;
-    var i = 0;
-    res.on('data', function(chunk){
-    	console.log("chunk " + i)
-    	i++
-    	JSONString = JSONString + chunk;
-    });
-    res.on('error', function(err) {
-    console.log(err)
-  });
-    res.on('end', function(chunk){
-    		console.log(JSONString);
-    		console.log("Start Parsing Data");
-    		var parsedjson = JSON.parse(JSONString);
-    		console.log(parsedjson.list[0]);
-		    var currentweather = parsedjson.list[0];
-		    var SensorObject = {
-		    	sensorname: "Current Temperature", 
-		    	sensorunits: "Celcius", 
-		    	value: TempConvert.k2c(currentweather.main.temp), 
-		    	DateTime: currentweather.dt};
-		    console.log("Parse Finish");
-			index.PostToMongoDB(SensorObject);
-    });
-
+  	var endpoint = "http://api.openweathermap.org";
+  	var path = "/data/2.5/forecast?id=" + cityid + "&APPID=" + apikey;
+	var options = {
+	    hostname: endpoint,
+	    path: path,
+	    method: 'GET',
+	    json:true
+	}
+request(options, function(error, response, body){
+    if(error){
+    	console.log(error);
+    } 
+    else {
+		console.log("Start Parsing Data");
+		var parsedjson = JSON.parse(body);
+		console.log(parsedjson.list[0]);
+	    var currentweather = parsedjson.list[0];
+	    var SensorObject = {
+	    	sensorname: "Current Temperature", 
+	    	sensorunits: "Celcius", 
+	    	value: TempConvert.k2c(currentweather.main.temp), 
+	    	DateTime: currentweather.dt};
+	    console.log("Parse Finish");
+		index.PostToMongoDB(SensorObject);
+    }
 });
-	
   }
 }
