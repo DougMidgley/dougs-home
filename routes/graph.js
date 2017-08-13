4b2bed5dbedfe46b3751a6b4d7916813b7ac17ae
Doug Midgley <douglasmidgley@gmail.com>
2017-08-13 13:45:33 +0200

    something

--- routes/graph.js
+++ routes/graph.js
@@ -66,6 +66,7 @@ router.get("/timeseries", function(req, res) {
 
 router.post("/timeseries", function(req, res) {
     var filtervalue = "";
+    console.log(req.body);
     if(req.body !== 'undefined' ){
         //filtervalue = req.body; 
         console.log(req.body);
