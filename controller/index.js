413e498a4eed6ac7fa27bee84317a478bb9f96a6
Doug Midgley <douglasmidgley@gmail.com>
2017-08-13 14:07:35 +0200

    something

--- controller/index.js
+++ controller/index.js
@@ -2,18 +2,24 @@ var mongoose = require('mongoose'),
     Model_Data = require("../Models/Model_Data.js");
 module.exports = {
   PostToMongoDB: function(JSONObject,response) {
-    var newRecord = new Model_Data(JSONObject);
+    try{
+            var newRecord = new Model_Data(JSONObject);
     console.log(newRecord);
     newRecord.save(function(err, res){
         console.log("res",res);
         if (err){
-        	console.log('Error on save!');
+            console.log('Error on save!');
             response.status(500).send("Error occurred on submitting data");
         } 
         else{
-        	console.log('saved');
-        	response.status(200).json(JSONObject);
+            console.log('saved');
+            response.status(200).json(JSONObject);
         }
     });
+    }
+    catch (ex){
+        console.log(ex);
+    }
+
   }
 }
